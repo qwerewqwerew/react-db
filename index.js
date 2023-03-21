8609682325bf4a63e4b303b24e1602489d32ef34
chase <53500545+m-kyu@users.noreply.github.com>
2023-03-21 15:38:46 +0900

    Port 변경

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const models = require("./models");
 const multer = require("multer");
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 const upload = multer({
 	storage: multer.diskStorage({
@@ -103,12 +103,12 @@ app.post("/purchase/:id", (req, res) => {
 	)
 		.then((result) => {
 			res.send({
-				result:true,
-			})
+				result: true,
+			});
 		})
 		.catch((error) => {
 			console.error(error);
-			res.status(500).send("에러가 발생했습니다")
+			res.status(500).send("에러가 발생했습니다");
 		});
 });
 
